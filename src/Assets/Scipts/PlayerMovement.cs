380f2a4495a8172d9d8a8986cf7e9ce8436ba68f
Natthan Krish <natthankrish@Neos-MacBook-Pro.local>
2024-05-01 23:48:17 +0700

    feat: player movement and weapon

--- src/Assets/Scipts/PlayerMovement.cs
+++ src/Assets/Scipts/PlayerMovement.cs
@@ -13,10 +13,12 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    public Animator animator;
  
     Vector3 velocity;
- 
     bool isGrounded;
+    bool isRunning;
+    float actualSpeed;
  
     // Update is called once per frame
     void Update()
@@ -27,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            animator.SetBool("Jumping", false);
         }
  
         float x = Input.GetAxis("Horizontal");
@@ -34,16 +37,33 @@ public class PlayerMovement : MonoBehaviour
  
         //right is the red Axis, foward is the blue axis
         Vector3 move = transform.right * x + transform.forward * z;
- 
-        controller.Move(move * speed * Time.deltaTime);
- 
+
+        // Check if running
+        isRunning = Input.GetButton("Run");
+        if (isRunning) {
+            actualSpeed = speed;
+            animator.SetBool("Running", true);
+        } else {
+            actualSpeed = speed/2;
+            animator.SetBool("Running", false);
+        }
+
+
+        controller.Move(move * actualSpeed * Time.deltaTime);
+        
         //check if the player is on the ground so he can jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             //the equation for jumping
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("Jumping", true);
         }
- 
+
+        if (move.sqrMagnitude > 0.01f){
+            animator.SetBool("Walking", true);
+        } else {
+            animator.SetBool("Walking", false);
+        }
         velocity.y += gravity * Time.deltaTime;
  
         controller.Move(velocity * Time.deltaTime);
