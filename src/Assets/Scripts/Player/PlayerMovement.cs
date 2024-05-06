5edc0c1f5b330eba551811400b5d841581dfaa10
Nathania Calista <nathania.calista01@gmail.com>
2024-05-06 12:38:55 +0700

    feat : speed orb

--- src/Assets/Scripts/Player/PlayerMovement.cs
+++ src/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,89 +1,88 @@
 ﻿using UnityEngine;
 using UnitySampleAssets.CrossPlatformInput;
+using System.Collections;
 
 namespace Nightmare
 {
     public class PlayerMovement : MonoBehaviour
-{
-    public CharacterController controller;
-    GameObject healthOrb;
-    PlayerHealth playerHealth;
-
-    public float speed = 12f;
-    public float gravity = -9.81f * 4;
-    public float jumpHeight = 3f;
- 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-    public Animator animator;
- 
-    Vector3 velocity;
-    bool isGrounded;
-    bool isRunning;
-    float actualSpeed;
-    
-    void Awake()
     {
-        healthOrb = GameObject.FindGameObjectWithTag("HealthOrb");
-        playerHealth = GetComponent<PlayerHealth>();
+        public CharacterController controller;
+        PlayerHealth playerHealth;
+
+        public float speed = 12f;
+        public float initialSpeed;
+        public float gravity = -9.81f * 4;
+        public float jumpHeight = 3f;
+
+        public Transform groundCheck;
+        public float groundDistance = 0.4f;
+        public LayerMask groundMask;
+        public Animator animator;
+
+        Vector3 velocity;
+        bool isGrounded;
+        bool isRunning;
+        float actualSpeed;
+
+        void Awake()
+        {
+            playerHealth = GetComponent<PlayerHealth>();
+            initialSpeed = speed; // Store the initial speed
+        }
+
+        void Update()
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+                animator.SetBool("Jumping", false);
+            }
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            isRunning = Input.GetButton("Run");
+            actualSpeed = isRunning ? speed : speed / 2;
+            animator.SetBool("Running", isRunning);
+            animator.SetBool("Walking", move.sqrMagnitude > 0.01f);
+
+            controller.Move(move * actualSpeed * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                animator.SetBool("Jumping", true);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Entered trigger with: " + other.gameObject.name);
+            if (other.CompareTag("HealthOrb"))
+            {
+                playerHealth.DrinkHealthOrb();
+                Destroy(other.gameObject); // Destroy the orb after use
+            }
+            else if (other.CompareTag("SpeedOrb"))
+            {
+                Debug.Log("Masuk ke attack");
+                StartCoroutine(TemporarySpeedBoost()); // Start coroutine for temporary speed boost
+                Destroy(other.gameObject); // Destroy the orb after use
+            }
+        }
+
+        IEnumerator TemporarySpeedBoost()
+        {
+            speed += 0.2f * initialSpeed;
+            yield return new WaitForSeconds(5f);
+            speed = initialSpeed;
+        }
     }
-    // Update is called once per frame
-    void Update()
-    {
-        //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
- 
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-            animator.SetBool("Jumping", false);
-        }
- 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
- 
-        //right is the red Axis, foward is the blue axis
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        // Check if running
-        isRunning = Input.GetButton("Run");
-        if (isRunning) {
-            actualSpeed = speed;
-            animator.SetBool("Running", true);
-        } else {
-            actualSpeed = speed/2;
-            animator.SetBool("Running", false);
-        }
-
-
-        controller.Move(move * actualSpeed * Time.deltaTime);
-        
-        //check if the player is on the ground so he can jump
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            //the equation for jumping
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            animator.SetBool("Jumping", true);
-        }
-
-        if (move.sqrMagnitude > 0.01f){
-            animator.SetBool("Walking", true);
-        } else {
-            animator.SetBool("Walking", false);
-        }
-        velocity.y += gravity * Time.deltaTime;
- 
-        controller.Move(velocity * Time.deltaTime);
-    }
-
-    void OnTriggerEnter (Collider other)
-    {
-        if(other.gameObject == healthOrb)
-        {
-            Debug.Log("Nabrak orb");
-            playerHealth.drinkHealthOrb();
-        }
-    } 
-}
 }
