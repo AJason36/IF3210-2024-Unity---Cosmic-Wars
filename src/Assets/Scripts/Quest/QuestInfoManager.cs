98e7ccca9441400d96464313e92076f584031314
Nathania Calista <nathania.calista01@gmail.com>
2024-05-10 00:57:48 +0700

    fix : game over

--- src/Assets/Scripts/Quest/QuestInfoManager.cs
+++ src/Assets/Scripts/Quest/QuestInfoManager.cs
@@ -16,7 +16,8 @@ public class QuestInfoManager : MonoBehaviour
   float remainingTime = 3f;
   float endOfTime = 0f;
   bool isDone;
-
+  bool hasRewarded;
+  
     // Start is called before the first frame update
     void Start()
     {
